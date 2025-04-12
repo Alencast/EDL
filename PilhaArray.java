8a0b63227765f9b9db23d8bb11e62ffc68a2f6b0
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-04-12 02:29:36 +0000

    comentários

--- PilhaArray.java
+++ PilhaArray.java
@@ -16,51 +16,51 @@ public class PilhaArray implements Pilha{
 
     public void push(Object o){
 
-        if(t >= capacidade - 1){
+        if(t >= capacidade - 1){ //se o topo for >= capacidade-1, a pilha está cheia
 
-            if(FC == 0){
+            if(FC == 0){ //se o fator de crescimento for igual a 0, a pilha dobra de tamanho por padrão
                
                 capacidade *= 2;
             }
             else{
-                capacidade += FC;
+                capacidade += FC; //se o fator de crescimento for diferente de zero, some FC à capacidade
             }
 
-            Object[] b = new Object[capacidade];
-            for(int i = 0; i < a.length; i++){
-                b[i] = a[i];
+            Object[] b = new Object[capacidade]; // é criado um novo array b, com a capacidade nova
+            for(int i = 0; i < a.length; i++){ // passa todos os elementos do array antigo para o novo
+                b[i] = a[i]; 
             }
-            a = b;
+            a = b; //ao final, a recebe o array novo que está em b
         }
 
-        a[++t] = o; //novo topo vai pro a
+        a[++t] = o; //novo topo vai pro a, lembrar que ++t != t++
     }
 
     public Object pop() throws PilhaVaziaExcecao{
 
-        if(isEmpty()){
-            throw new PilhaVaziaExcecao("Pilha vazia");
+        if(isEmpty()){ //verifica se a lista está vazia
+            throw new PilhaVaziaExcecao("Pilha vazia"); // caso esteja, lança a exception
         }
 
-        return a[t--]; //pega o topo e remove (lembrar que t-- != --t)
+        return a[t--]; //pega o topo e remove a referência, ou seja, o valor ainda está lá, mas não é possível acessá-lo (lembrar que t-- != --t)
 
     }
 
     public Object top() throws PilhaVaziaExcecao{
 
-        if (isEmpty()){
-            throw new PilhaVaziaExcecao("Pilha vazia");
+        if (isEmpty()){ //verifica se a lista está vazia
+            throw new PilhaVaziaExcecao("Pilha vazia"); //caso esteja, retorna o exception
         }
         
-        return a[t];
+        return a[t]; //retorna o array a no indice t(topo)
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty(){// verifica se a lista está vazia
         
-        return t == -1;
+        return t == -1; //retorna um boolean, fazendo a seguinte pergunta: t é igual a -1? se o topo for igual a -1, então a lista está vazia
     }
 
-    public int size(){
+    public int size(){ //retorna o tamanho da lista
         return t + 1;
     }
 
