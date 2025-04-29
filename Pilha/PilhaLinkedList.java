973b084b8026ba46d7f5ed74661034648a2bdba8
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-04-29 03:31:10 +0000

    dual linked list

--- Pilha/PilhaLinkedList.java
+++ Pilha/PilhaLinkedList.java
@@ -30,3 +30,37 @@ public class PilhaLinkedList {
     
     
 }
+// class No {
+//     int valor;
+//     No next;
+//     No prev;
+    
+//     public No(int valor) {
+//         this.valor = valor;
+//         this.next = null;
+//         this.prev = null;
+//     }
+// }
+
+// class Pilha {
+//     No topo;
+//     No tail;
+    
+//     public Pilha() {
+//         topo = null;
+//         tail = null;
+//     }
+    
+//   
+//     public void push(int valor) {
+//         No novo = new No(valor);
+        
+//         if (tail == null) { // vazia
+//             topo = novo;
+//             tail = novo;
+//         } else {
+//             tail.next = novo;  
+//             novo.prev = tail;  
+//             tail = novo;       
+//         }
+//     }
