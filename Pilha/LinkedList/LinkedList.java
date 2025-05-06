f48d733f535befc2bd2e3f0657d38e9f3310c34a
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-05-06 03:23:15 +0000

    linked list

--- Pilha/LinkedList/LinkedList.java
+++ Pilha/LinkedList/LinkedList.java
@@ -1,49 +1,98 @@
-package LinkedList;
-
-public class LinkedList {
-
-    private class Node {
+public class LinkedList{
+    
+    private class Node{
+        
         int value;
         Node prev;
         Node next;
-
-        Node(int value) {
+        
+        Node(int value){
             this.value = value;
             this.prev = null;
             this.next = null;
         }
-
-        public Node getPrev() {
+        
+        public Node getPrev(){
             return prev;
         }
-
-        public void setPrev(Node prev) {
+        
+        public void setPrev(Node prev){
             this.prev = prev;
         }
-
-        public Node getNext() {
+        
+        public Node getNext(){
             return next;
         }
-
-        public void setNext(Node next) {
+        
+        public void setNext(Node next){
             this.next = next;
         }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
+        
     }
-
+        
     private Node head;
     private Node tail;
-
-    public LinkedList(){
-        this.head = null;
-        this.tail = null;
+    
+    //Métodos genéricos
+    public int size(){
+        int contador = 0;
+        Node no_atual = head;
+        while(no_atual != null){
+            no_atual = no_atual.getNext();
+            contador++;
+        }
+        return contador;
     }
+    
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+    
+    //Métodos de fila
+    public boolean isFirst(int value){
+        return value == head.value;
+    }
+    
+    public boolean isLast(int value){
+        return value == tail.value;
+    }
+    
+    //Métodos para acessar
+    public int first(){
+        return head.value;
+    }
+    
+    public int last(){
+        return tail.value;
+    }
+    
+    public Node before(Node p){
+        
+        Node no_atual = head;
+        
+        while(no_atual.getNext() != p){ // O que fazer se a lista estiver vazia?
+            no_atual = no_atual.getNext();
+            
+            
+        }
+        
+        return no_atual;
+    }
+    
+    public Node after(Node p){
+        
+        Node no_atual = head;
+        
+        while(no_atual != p){
+            no_atual = no_atual.getNext();
+        }
+        return no_atual.getNext();
+    }
+    
+    
+    
+    
 
+        
+    
 }
