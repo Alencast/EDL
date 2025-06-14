d1e7c76380d6a6e6186c949dfa276312e090f8b8
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-06-14 00:26:31 -0300

    Update Pilha.java

--- Fila/Duvidas/Pilha.java
+++ Fila/Duvidas/Pilha.java
@@ -1,72 +1,53 @@
-public class Pilha{
+public class Pilha{ 
     
     private int capacidade;
     private int tamanho;
-    private Object[] a;
+    private Object a[];
     private int fim;
     
-    
-    public Pilha(int capacidade){
-        
+    public Pilha(int capacidade){ 
         this.capacidade = capacidade;
         this.tamanho = 0;
         this.a = new Object[capacidade];
         this.fim = -1;
     }
     
-    private boolean isFull(){
-        
+    public boolean isFull(){ 
         return capacidade == tamanho;
     }
     
-    private boolean isEmpty(){
+    public boolean isEmpty(){ 
         return tamanho == 0;
     }
     
-    public void empilhar(Object o){
+    public void empilhar(Object o){ 
         
-        if(isFull()){
+        if(isFull()){ 
             
             int novacap = capacidade * 2;
             Object[] b = new Object[novacap];
             
-            for(int i = 0; i <= fim; i++){
-                
+            for(int i = 0; i <= fim; i++){ 
                 b[i] = a[i];
-            }
-            
+            } 
             a = b;
             capacidade = novacap;
         }
         
         a[++fim] = o;
-        tamanho ++;
+        tamanho++;
     }
     
-    public Object desempilhar(){
-        
-        if(isEmpty()){
+    public Object desempilhar(){ 
+        if(isEmpty()){ 
             
-           throw new IllegalArgumentException("A pilha está vazia");
+            throw new IllegalArgumentException("Pilha vazia");
         }
         
         Object removido = a[fim];
         a[fim--] = null;
         tamanho--;
-
+        
         return removido;
     }
-    
-    public Object topo(){
-        
-        if(isEmpty()){
-            throw new IllegalArgumentException("Pilha vazia");
-        }    
-    
-    return a[fim];
-    
-    }
-    
-
-    
 }
